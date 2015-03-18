2a5d60deaf3523e72b7f3286d8fb353b76f7863f
Michael Haberler <michael.haberler@ims.co.at>
2015-03-18 15:14:28 +0100

    Open files and additional window for each file.

--- mpl_qt/__init__.py
+++ mpl_qt/__init__.py
@@ -1,2 +1,12 @@
+"""
+http://stackoverflow.com/questions/1736015/debugging-a-pyqt4-app
+"""
+
+def debug_trace():
+  '''Set a tracepoint in the Python debugger that works with Qt'''
+  from PyQt4.QtCore import pyqtRemoveInputHook
+  from ipdb import set_trace
+  pyqtRemoveInputHook()
+  set_trace()
 
 __version__ = "0.1"
--- mpl_qt/main.py
+++ mpl_qt/main.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 import sys
 from PyQt4 import QtGui
 from PyQt4 import Qt
+from PyQt4 import QtCore
 
 import numpy as np
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
@@ -12,6 +13,7 @@ from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg
 from matplotlib.figure import Figure
 
 import mpl_qt.ui.main as uimain
+import mpl_qt.ui.inputdata as uiinput
 
 
 class NavigationToolbar(NavigationToolbar2QTAgg):
@@ -86,12 +88,17 @@ class PlotWidget(QtGui.QWidget):
         QtGui.QMessageBox.information(self, "Click!", msg)
 
 
-class MainWindow(QtGui.QMainWindow, uimain.Ui_MainWindow):
+class InputDataWindow(QtGui.QMainWindow, uiinput.Ui_MainWindow):
+    """
+    .. todo::
+        QWidget or QMainWindow?
+    """
 
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
+        super(InputDataWindow, self).__init__(parent)
         self.setupUi(self)
 
+        # plot tabs for grids
         self.tabwidget = QtGui.QTabWidget(parent=self)
         self.plots = {
             'p1': PlotWidget(parent=self.tabwidget),
@@ -99,17 +106,85 @@ class MainWindow(QtGui.QMainWindow, uimain.Ui_MainWindow):
         }
         for tabname, tabplot in self.plots.iteritems():
             self.tabwidget.addTab(tabplot, tabname)
-
         self.horizontalLayout.insertWidget(0, self.tabwidget)
 
-        data = {'col1':['1','2','3'], 'col2':['4','5','6'], 'col3':['7','8','9']}
-        headers = []
-        for i, (name, value_list) in enumerate(data.iteritems()):
-            headers.append(name)
-            for j, value in enumerate(value_list):
-                item = QtGui.QTableWidgetItem(value)
-                self.gridTableWidget.setItem(j, i, item)
-                print(i, j, name, value)
+
+class InputFile(object):
+
+    def __init__(self, filename, multiplier, window_parent=None):
+        self.filename = filename
+        self.multiplier = multiplier
+        self.window = InputDataWindow(parent=window_parent)
+
+
+class MainWindow(QtGui.QMainWindow, uimain.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+        self.actionOpen.triggered.connect(self.open)
+
+        # containers for file data
+        self.input_files = []
+
+        # plot tabs for grids
+        self.tabwidget = QtGui.QTabWidget(parent=self)
+        self.plots = {
+            'p1': PlotWidget(parent=self.tabwidget),
+            'p2': PlotWidget(parent=self.tabwidget)
+        }
+        for tabname, tabplot in self.plots.iteritems():
+            self.tabwidget.addTab(tabplot, tabname)
+        self.horizontalLayout.insertWidget(0, self.tabwidget)
+
+        # table for grids
+        headers = ['Multiplier', 'Filename']
+        self.gridTableWidget.setColumnCount(2)
         self.gridTableWidget.setHorizontalHeaderLabels(headers)
+        self.gridTableWidget.setSelectionBehavior(
+            Qt.QAbstractItemView.SelectRows)
+        self.gridTableWidget.itemDoubleClicked.connect(self.table_item_clicked)
+
+        self.update_ui()
+
+    def update_ui(self):
+
+        self.gridTableWidget.setRowCount(len(self.input_files))
+        for i, input_data in enumerate(self.input_files):
+            item = QtGui.QTableWidgetItem(str(input_data.multiplier))
+            self.gridTableWidget.setItem(i, 0, item)
+            item = QtGui.QTableWidgetItem(input_data.filename)
+            item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
+            self.gridTableWidget.setItem(i, 1, item)
+            print(i, input_data.filename)
+
+        #self.gridTableWidget.setVerticalHeaderLabels(self.data.keys())
         self.gridTableWidget.resizeColumnsToContents()
         self.gridTableWidget.resizeRowsToContents()
+
+    @QtCore.pyqtSlot()
+    def open(self):
+        filename = QtGui.QFileDialog.getOpenFileName(
+            self, 'Open File', '.')
+            #self, 'Open File', '.', '(*.bin)')
+
+        if filename:
+            self.input_files.append(InputFile(filename, +1.0, self))
+            self.input_files[-1].window.show()
+            self.update_ui()
+
+    @QtCore.pyqtSlot(Qt.QTableWidgetItem)
+    def table_item_clicked(self, item):
+        selected_text = item.text()
+        try:
+            input_file = next(item for item in self.input_files
+                              if item.filename == selected_text)
+        except StopIteration as e:
+            print("Couldn't find selected item \"{}\".".format(selected_text))
+            return
+
+        if input_file.window.isHidden():
+            input_file.window.show()
+        else:
+            print(input_file, " is already visible")
